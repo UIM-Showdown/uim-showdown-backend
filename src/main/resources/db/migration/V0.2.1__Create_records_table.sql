b99156123a3ddcc96ea2b4209c95bfa096f9d3a4
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-03-21 17:08:26 -0700

    Fix character length mismatch in `records` table

--- src/main/resources/db/migration/V0.2.1__Create_records_table.sql
+++ src/main/resources/db/migration/V0.2.1__Create_records_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `records` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `description` VARCHAR(16) NOT NULL,
-    `skill` VARCHAR(512) NOT NULL,
+    `description` VARCHAR(512) NOT NULL,
+    `skill` VARCHAR(16) NOT NULL,
     PRIMARY KEY (`id`)
 );
