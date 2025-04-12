645d2710316bff75b9477c497d02bff6c3aa2734
kuhnertdm <kuhnertdm@gmail.com>
2025-04-12 18:19:05 -0400

    Add submission create endpoints

--- src/main/java/org/uimshowdown/bingo/repositories/RecordHandicapRepository.java
+++ src/main/java/org/uimshowdown/bingo/repositories/RecordHandicapRepository.java
@@ -1,8 +1,11 @@
 package org.uimshowdown.bingo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.uimshowdown.bingo.models.RecordHandicap;
 
 public interface RecordHandicapRepository extends CrudRepository<RecordHandicap, Integer> {
     Iterable<RecordHandicap> findAllByRecordId(int recordId);
+    Optional<RecordHandicap> findByName(String name);
 }
