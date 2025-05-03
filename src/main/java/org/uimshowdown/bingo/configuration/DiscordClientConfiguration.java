3260d7d0380788ee9de20e2b7b40b29f686530a0
kuhnertdm <kuhnertdm@gmail.com>
2025-05-03 11:58:08 -0400

    Add missing file

---
+++ src/main/java/org/uimshowdown/bingo/configuration/DiscordClientConfiguration.java
@@ -0,0 +1,28 @@
+package org.uimshowdown.bingo.configuration;
+
+import java.util.EnumSet;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+@Configuration
+public class DiscordClientConfiguration {
+    
+    @Value("${discord.token}")
+    private String token;
+    
+    @Bean
+    public JDA discordClient() {
+        return JDABuilder.createLight(token)
+            .enableIntents(EnumSet.of(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT))
+            .setMemberCachePolicy(MemberCachePolicy.ALL)
+            .build();
+    }
+
+}
